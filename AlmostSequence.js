90a9a7fd9f6e8deae89239058a2fd25c92271b3e
Pete Sword <pete.sword@yahoo.com>
2018-08-11 23:35:01 -0600

    almost sequence

---
+++ AlmostSequence.js
@@ -0,0 +1,23 @@
+//Given a sequence of integers as an array, determine whether it is possible to obtain a strictly increasing sequence 
+//by removing no more than one element from the array.
+
+almostIncreasingSequence = (sequence) => {
+  let found = false
+  for(i = 0; i <= sequence.length; i++) {
+    if (sequence[i] <= sequence[i-1]) {
+      if(found) {
+        return false
+      }
+      found = true
+          
+      if(i === 1 || i + 1 === sequence.length) {
+        continue
+      } else if (sequence[i] > sequence[i -2]) {
+        sequence[i - 1] = sequence[i - 2]
+      } else if(sequence[i-1] >= sequence[i+1]) {
+        return false;
+      }
+    }
+  }
+  return true
+}
